8b76da10385131baf11884ecea3dfc065d9adfa7
Gautam Nair <4gm20ai013@gmit.ac.in>
2024-03-23 21:02:00 +0530

    Add files via upload
    n

---
+++ src/components/Header.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let y;
+
+    export let tabs = [
+        { name: "Projects ", link: "#projects" },
+        { name: "About me", link: "#about" },
+        { name: "Internship", link: "https://goku0090.github.io/Internship_page/work%20Experince_1" },
+        { name: "Resume", link: "https://heyzine.com/flip-book/cbdaffd4d3.html" },
+        // { name: "Contact", link: "" },
+        // {name: '', link: ''},
+    ];
+</script>
+
+<header
+    class={"static z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
+        (y > 0
+            ? " py-4 bg-slate-950 border-violet-950"
+            : " py-6 bg-transparent border-transparent")}
+>
+    <h1 class="font-medium">
+        <b class="font-bold poppins">Gautam</b> <span class="">Nair</span>
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a
+                href={tab.link}
+                class="duration-200 hover:text-violet-400"
+                target={index === 2 ? "_blank" : ""}
+            >
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+        <button 
+            onclick="window.open('mailto:gautamnair94@gmail.com', '_blank')" 
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Get in touch</h4>
+        </button>
+    </div>
+</header>
